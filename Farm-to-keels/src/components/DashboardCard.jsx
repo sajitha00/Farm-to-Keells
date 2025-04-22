047b71c2530cc0114be97a686441be34c4321bd9
sajitha00 <sajithageevinda.sd@gmail.com>
2025-04-22 20:34:57 +0530

    add some ui improvements

--- Farm-to-keels/src/components/DashboardCard.jsx
+++ Farm-to-keels/src/components/DashboardCard.jsx
@@ -4,8 +4,8 @@ const DashboardCard = ({ icon, title, onClick, badge }) => {
   return (
     <div className="relative">
       <div
-        className="flex flex-col items-center justify-center p-6 bg-white rounded-2xl shadow-lg hover:shadow-xl transition cursor-pointer"
         onClick={onClick}
+        className="flex flex-col items-center justify-center p-6 bg-white rounded-2xl shadow-lg hover:shadow-xl transition-transform duration-300 transform hover:scale-105 cursor-pointer"
       >
         <img src={icon} alt={title} className="w-16 h-16 mb-3" />
         <p className="text-lg font-semibold">{title}</p>
