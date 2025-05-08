5c9660132b3f15039a0d7dab13bd0afa33a7a2f2
sajitha00 <sajithageevinda.sd@gmail.com>
2025-05-08 17:35:09 +0530

    styling

--- Farm-to-keels/src/App.jsx
+++ Farm-to-keels/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import FarmerRegister from "./pages/FarmerRegister";
